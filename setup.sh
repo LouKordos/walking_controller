0f4fa0f3e0af953aaf120d1d2fc4ecf83482ca34
Loukas K. (P53) <tuloukas@gmail.com>
2020-04-06 16:14:31 +0200

    Create build directory before changing to it (this is needed in case of initial build)

--- setup.sh
+++ setup.sh
@@ -216,7 +216,7 @@ cmake ..
 make -j 12
 
 echo -e "\n#This alias will allow easier Simulation startup." >> ${HOME}/.bashrc
-echo -e "\nalias start_biped_simulation=\"cd ${HOME}/.gazebo/models/simplified_biped/control_plugin/build/ && make && gazebo --verbose ../../simplified_biped.world\"" >> ${HOME}/.bashrc
+echo -e "\nalias start_biped_simulation=\"mkdir ${HOME}/.gazebo/models/simplified_biped/control_plugin/build/ ; cd ${HOME}/.gazebo/models/simplified_biped/control_plugin/build/ && make && gazebo --verbose ../../simplified_biped.world\"" >> ${HOME}/.bashrc
 
 eval "$(cat ${HOME}/.bashrc | tail -n +10)" # https://askubuntu.com/questions/64387/cannot-successfully-source-bashrc-from-a-shell-script
 
@@ -232,7 +232,7 @@ cmake ..
 make -j 12
 
 echo -e "\n#This alias will allow easier startup of the Biped walking controller." >> ${HOME}/.bashrc
-echo -e "\nalias run_walking_controller=\"cd ${WORKSPACE_DIRECTORY}/build && make && ./controller\"" >> ${HOME}/.bashrc
+echo -e "\nalias run_walking_controller=\"mkdir ${WORKSPACE_DIRECTORY}/build/ cd ${WORKSPACE_DIRECTORY}/build/ && make && ./controller\"" >> ${HOME}/.bashrc
 
 echo -e "\n#This alias will (hopefully) allow updating all Biped repos automatically." >> ${HOME}/.bashrc
 echo -e "\nalias update_biped_repos=\"cd ${WORKSPACE_DIRECTORY} && git pull ; cd ${GITHUB_DIRECTORY}/jupyter_notebooks/ && git pull ; cd ${HOME}/.gazebo/models/simplified_biped/ && git pull\"" >> ${HOME}/.bashrc
