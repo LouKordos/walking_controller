ed14fcad4551aca7dc184bb5db3ca9a6542d16dc
Loukas K. (Arch Desktop) <loukordos@protonmail.com>
2021-05-21 10:56:27 +0200

    Run gazebo command specifically, kill docker container and gzserver directly

--- utils/start_sim.py
+++ utils/start_sim.py
@@ -18,9 +18,20 @@ tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_client.connect((host_ip, server_port))
 
 controller_process = subprocess.Popen("docker run -v $HOME/dev/walking_controller/plot_data:/plot_data -e \"IS_DOCKER=Y\" -e \"TERM=xterm-256color\" -it --net=host loukordos/walking_controller:develop", shell=True)
-time.sleep(0.1)
-sim_process = subprocess.Popen("./run_sim", cwd="/home/loukas/.gazebo/models/simplified_biped/", shell=True)
+print(controller_process.pid)
 
+env = {
+    **os.environ,
+    "LD_LIBRARY_PATH":"/home/loukas/.gazebo/models/simplified_biped/control_plugin/build/"
+}
+
+time.sleep(0.1)
+print("this ran")
+# sim_process = subprocess.Popen("taskset -c 8-11,20-23 gazebo -e bullet simplified_biped.world", cwd="/home/loukas/.gazebo/models/simplified_biped/", env=env, shell=True)
+sim_process = subprocess.Popen("LD_LIBRARY_PATH=/home/loukas/.gazebo/models/simplified_biped/control_plugin/build/ chrt -r 1 taskset -c 8-11,20-23 gazebo -e bullet simplified_biped.world", cwd="/home/loukas/.gazebo/models/simplified_biped/", shell=True, stdout=subprocess.PIPE)
+# output, error = sim_process.communicate()
+# print(output, error)
+print("this ran too")
 # Read data from the TCP server and close the connection
 while True:
     try:
@@ -31,16 +42,24 @@ while True:
             print("Reset trigger received in python.")
             controller_process.send_signal(signal.SIGINT)
 
-            ps_command = subprocess.Popen("ps -o pid --ppid %d --noheaders" % sim_process.pid, shell=True, stdout=subprocess.PIPE)
-            ps_output = ps_command.stdout.read()
-            os.kill(int(ps_output), signal.SIGINT)
-            sim_process.send_signal(signal.SIGINT)
+            subprocess.Popen("docker kill $(docker ps --quiet --filter ancestor=loukordos/walking_controller:develop)", shell=True)
 
-            time.sleep(2.5)
+            # ps_command = subprocess.Popen("ps -o pid --ppid %d --noheaders" % sim_process.pid, shell=True, stdout=subprocess.PIPE)
+            # ps_output = ps_command.stdout.read()
+            # print("Gazebo PID:", int(ps_output))
+            # os.kill(int(ps_output), signal.SIGINT)
+            # sim_process.send_signal(signal.SIGKILL)
+            kill_process = subprocess.Popen("killall gzserver", shell=True, stdout=subprocess.PIPE)
+            output, error = kill_process.communicate()
+
+            # kill_process = subprocess.Popen("killall gazebo", shell=True, stdout=subprocess.PIPE)
+            # output, error = kill_process.communicate()
+
+            time.sleep(3)
 
             controller_process = subprocess.Popen("docker run -v $HOME/dev/walking_controller/plot_data:/plot_data -e \"IS_DOCKER=Y\" -e \"TERM=xterm-256color\" -it --net=host loukordos/walking_controller:develop", shell=True)
             time.sleep(0.1)
-            sim_process = subprocess.Popen("./run_sim", cwd="/home/loukas/.gazebo/models/simplified_biped/", shell=True)
+            sim_process = subprocess.Popen("LD_LIBRARY_PATH=/home/loukas/.gazebo/models/simplified_biped/control_plugin/build/ chrt -r 1 taskset -c 8-11,20-23 gazebo -e bullet simplified_biped.world", cwd="/home/loukas/.gazebo/models/simplified_biped/", shell=True, stdout=subprocess.PIPE)
 
             time.sleep(4.5)
 
@@ -65,9 +84,8 @@ while True:
     except KeyboardInterrupt:
         print("Caught interrupt, stopping controller and sim...")
         controller_process.send_signal(signal.SIGINT)
+        subprocess.Popen("docker kill $(docker ps --quiet --filter ancestor=loukordos/walking_controller:develop)", shell=True)
 
-        ps_command = subprocess.Popen("ps -o pid --ppid %d --noheaders" % sim_process.pid, shell=True, stdout=subprocess.PIPE)
-        ps_output = ps_command.stdout.read()
-        os.kill(int(ps_output), signal.SIGINT)
-        sim_process.send_signal(signal.SIGINT)
+        kill_process = subprocess.Popen("killall gzserver", shell=True, stdout=subprocess.PIPE)
+        output, error = kill_process.communicate()
         sys.exit()
