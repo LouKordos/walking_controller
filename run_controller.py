15f8db09f2788a5515ef54019925681cfec9bc92
Loukas (P53-Arch) <tuloukas@gmail.com>
2022-01-09 16:13:54 +0100

    Fix formatting

--- run_controller.py
+++ run_controller.py
@@ -10,13 +10,14 @@ parser.add_argument("--skip_pinning", help="Set environment variable SKIP_PINNIN
 
 parser.add_argument('--real-time-factor', '--rtf', type=float, default=1.0, help='Set real-time-factor. This makes the controller run at [RTF] times realtime.')
 parser.add_argument('--runtime-limit', '--rtl', type=int, default=-1, help='Set runtime limit. This makes the controller exit after the specified time has elapsed.')
+
 args = parser.parse_args()
 
-print("Args:", args)
+print("Args:", args, "\n")
 
 command = f"time docker build -t loukordos/walking-controller:$(git rev-parse --abbrev-ref HEAD)-ubuntu -f Dockerfile.ubuntu . && docker run -v $HOME/dev/walking_controller/plot_data:/plot_data -e \"RUNTIME_LIMIT={args.runtime_limit}\" -e \"RTF={args.real_time_factor}\" -e \"USE_WEB_UI={int(args.use_web_ui)}\" -e \"SKIP_PINNING={int(args.skip_pinning)}\" -e \"IS_LAPTOP={int(args.laptop)}\" -e \"TERM=xterm-256color\" -it --net=host loukordos/walking-controller:$(git rev-parse --abbrev-ref HEAD)-ubuntu"
 
-print("Running command:", command)
+print("Running command:", command, "\n")
 
 os.system(command)
 
