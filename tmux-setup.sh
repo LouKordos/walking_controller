fb1005af87bdf31695dca1329ee2545b0e09839f
Loukas K. (Arch Desktop) <loukordos@protonmail.com>
2022-03-23 23:58:24 +0100

    Fix walking controller repo path

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -2,7 +2,7 @@
 
 tmux new -s biped -d
 tmux rename-window -t biped biped-controller
-tmux send-keys -t biped 'cd ~/dev/walking_controller/ && clear' C-m
+tmux send-keys -t biped 'cd ~/Nextcloud/dev/walking_controller/ && clear' C-m
 tmux split-window -h -t biped
 tmux select-window -t biped:1
 tmux rename-window -t biped biped-sim
