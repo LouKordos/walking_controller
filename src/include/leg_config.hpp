d4c4c229c38926d1de7c541663147e5c122dd774
Loukas K. (Arch Desktop) <loukordos@protonmail.com>
2022-03-30 22:53:02 +0200

    Update leg config based on CAD model

--- src/include/leg_config.hpp
+++ src/include/leg_config.hpp
@@ -4,70 +4,69 @@
 struct leg_config {
     //Inertia tensors for each link around CoM
    
-    double Ixx_hip3 = 0.004505;
+    double Ixx_hip3 = 0.007;
     double Ixy_hip3 = 0;
     double Ixz_hip3 = 0;
 
     double Iyx_hip3 = 0;
-    double Iyy_hip3 = 0.004505;
+    double Iyy_hip3 = 0.007;
     double Iyz_hip3 = 0;
 
     double Izx_hip3 = 0;
     double Izy_hip3 = 0;
-    double Izz_hip3 = 0.008515;
+    double Izz_hip3 = 0.012;
 
-    double Ixx_hip2 = 0.004505;
+    double Ixx_hip2 = 0.007;
     double Ixy_hip2 = 0;
     double Ixz_hip2 = 0;
 
     double Iyx_hip2 = 0;
-    double Iyy_hip2 = 0.008515;
+    double Iyy_hip2 = 0.012;
     double Iyz_hip2 = 0;
 
     double Izx_hip2 = 0;
     double Izy_hip2 = 0;
-    double Izz_hip2 = 0.004505;
+    double Izz_hip2 = 0.007;
 
-
-    double Ixx_hip1 = 0.0017;
+    double Ixx_hip1 = 0.0017 * 2;
     double Ixy_hip1 = 0;
     double Ixz_hip1 = 0;
 
     double Iyx_hip1 = 0;
-    double Iyy_hip1 = 0.022;
+    double Iyy_hip1 = 0.022 * 2;
     double Iyz_hip1 = 0;
 
     double Izx_hip1 = 0;
     double Izy_hip1 = 0;
-    double Izz_hip1 = 0.022;
+    double Izz_hip1 = 0.022 * 2;
 
     // "ul" is upper leg
 
-    double Ixx_ul = 0.008673;
+    double Ixx_ul = 0.052;
     double Ixy_ul = 0;
     double Ixz_ul = 0;
 
     double Iyx_ul = 0;
-    double Iyy_ul = 0.008673;
+    double Iyy_ul = 0.05;
     double Iyz_ul = 0;
 
     double Izx_ul = 0;
     double Izy_ul = 0;
-    double Izz_ul = 2.436e-04;
+    double Izz_ul = 0.007;
 
     //"ll" is lower leg
 
-    double Ixx_ll = 0.005;
+    double Ixx_ll = 0.015;
     double Ixy_ll = 0;
     double Ixz_ll = 0;
 
     double Iyx_ll = 0;
-    double Iyy_ll = 0.005;
+    double Iyy_ll = 0.014;
     double Iyz_ll = 0;
 
     double Izx_ll = 0;
     double Izy_ll = 0;
-    double Izz_ll = 2.078e-04;
+    double Izz_ll = 0.001;
 
     double Ixx_foot = 2.733e-04;
     double Ixy_foot = 0;
@@ -83,11 +82,11 @@ struct leg_config {
 
     // Masses of each link in kg
 
-    double m_hip3 = 2.5;
-    double m_hip2 = 2.5;
-    double m_hip1 = 5;
-    double m_ul = 0.6;
-    double m_ll = 0.5;
+    double m_hip3 = 4;
+    double m_hip2 = 4;
+    double m_hip1 = 8;
+    double m_ul = 2.1;
+    double m_ll = 0.83;
     double m_foot = 0.1;
 
     // Distances in transformation matrices. lx_com is from the previous joint frame to the next CoM frame.
@@ -105,7 +104,7 @@ struct leg_config {
     double l2_z = 0.117;
     double l2_x_com = 0;
     double l2_y_com = -0.12;
-    double l2_z_com = 0;
+    double l2_z_com = 0.117;
 
     double l3_x = 0;
     double l3_y = 0.12;
