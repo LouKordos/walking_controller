fb266c0f3538cfc101edd5ef61d32f04845d8732
Loukas K. (P53) <tuloukas@gmail.com>
2022-03-30 11:51:04 +0200

    Add debug option

--- run_controller.py
+++ run_controller.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser(description='Wrapper script for running the bip
 parser.add_argument("--laptop", "-l", help="Set environment variable IS_LAPTOP=1, e.g. to adjust CPU pinning.", action="store_true")
 parser.add_argument("--use_web_ui", help="Set environment variable USE_WEB_UI=1, to disable receiving commands from Web UI.", action="store_true")
 parser.add_argument("--skip_pinning", help="Set environment variable SKIP_PINNING=1, to skip CPU pinning ", action="store_true")
+parser.add_argument("--debug", help="Compile for debugging and start with gdb", action="store_true")
 
 parser.add_argument('--real-time-factor', '--rtf', type=float, default=1.0, help='Set real-time-factor. This makes the controller run at [RTF] times realtime.')
 parser.add_argument('--runtime-limit', '--rtl', type=int, default=-1, help='Set runtime limit. This makes the controller exit after the specified time has elapsed.')
@@ -16,6 +17,9 @@ args = parser.parse_args()
 print("Args:", args, "\n")
 
 command = f"time docker build -t loukordos/walking-controller:$(git rev-parse --abbrev-ref HEAD)-ubuntu -f Dockerfile.ubuntu . && docker run -v $HOME/Nextcloud/dev/walking_controller/plot_data:/plot_data -e \"RUNTIME_LIMIT={args.runtime_limit}\" -e \"RTF={args.real_time_factor}\" -e \"USE_WEB_UI={int(args.use_web_ui)}\" -e \"SKIP_PINNING={int(args.skip_pinning)}\" -e \"IS_LAPTOP={int(args.laptop)}\" -e \"TERM=xterm-256color\" -it --net=host loukordos/walking-controller:$(git rev-parse --abbrev-ref HEAD)-ubuntu"
+
+if args.debug:
+    command += " sh -c 'cmake .. -DCMAKE_BUILD_TYPE=Debug && make -j && gdb controller'"
 
 print("Running command:", command, "\n")
 
