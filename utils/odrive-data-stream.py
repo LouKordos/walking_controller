5a51080632eb920f12f446397e01c2971108d29b
Loukas K. (P53) <tuloukas@gmail.com>
2022-03-30 14:33:20 +0200

    Update zero offsets after leg rebuild, slight UDP cosmestic changes

--- utils/odrive-data-stream.py
+++ utils/odrive-data-stream.py
@@ -9,12 +9,13 @@ import signal,sys,time
 import socket
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 42068
+UDP_PORT = 4200
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 
-sock.bind((UDP_IP, UDP_PORT))
+sock.bind(("127.0.0.1", 42068))
+# sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, 1)
 
 terminate = False
 
@@ -40,23 +41,23 @@ print("Waiting for Knee servo to be connected...")
 knee_servo = odrive.find_any(serial_number=str(hex(56557058142768).split('x')[-1]).upper()) # Knee
 print("Knee connected.")
 
-hip_3_zero_offset = 0.2002742886543274 # in turns
-hip_2_zero_offset = -0.457975834608078 # in turns
-hip_1_zero_offset = -0.016931354999542236 # in turns
-knee_zero_offset = -0.04928791522979736 # in turns
+hip_3_zero_offset = 0 # in turns
+hip_2_zero_offset = 0 # in turns
+hip_1_zero_offset = 0 # in turns
+knee_zero_offset = 0 # in turns
 
-hip_3_servo.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-hip_2_servo.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-hip_1_servo.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-knee_servo.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+# hip_3_servo.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+# hip_2_servo.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+# hip_1_servo.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+# knee_servo.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
-hip_3_servo.axis0.controller.input_pos = hip_3_zero_offset
-hip_2_servo.axis0.controller.input_pos = hip_2_zero_offset
-hip_1_servo.axis0.controller.input_pos = hip_1_zero_offset
-knee_servo.axis0.controller.input_pos = knee_zero_offset
+# hip_3_servo.axis0.controller.input_pos = hip_3_zero_offset
+# hip_2_servo.axis0.controller.input_pos = hip_2_zero_offset
+# hip_1_servo.axis0.controller.input_pos = hip_1_zero_offset
+# knee_servo.axis0.controller.input_pos = knee_zero_offset
 
 hip_3_lower_limit = -0.48 # in rad
-hip_2_lower_limit = -0.8 # in rad
+hip_2_lower_limit = -0.4 # in rad
 hip_1_lower_limit = -0.4 # in rad
 knee_lower_limit = -1.2 # in rad
 
@@ -69,6 +70,9 @@ gear_ratio = 1/8 # planetary gearbox ratio
 
 # plt.ion()
 # plt.show()
+
+message = "{theta1}|{theta2}|{theta3}|{theta4}|0|{theta1dot}|{theta2dot}|{theta3dot}|{theta4dot}|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0".format(theta1=0, theta2=0, theta3=0, theta4=0, theta1dot=0, theta2dot=0, theta3dot=0, theta4dot=0)
+sock.sendto(message.encode(), (UDP_IP, UDP_PORT))
 
 data = []
 
@@ -91,6 +95,9 @@ while True:
     theta3dot_raw = hip_1_servo.encoder_estimator0.vel_estimate
     theta4dot_raw = knee_servo.encoder_estimator0.vel_estimate
 
+    # theta1_raw = theta2_raw = theta3_raw = theta4_raw = 0.1
+    # theta1dot_raw = theta2dot_raw = theta3dot_raw = theta4dot_raw = -0.05
+
     theta1dot = 2 * math.pi * gear_ratio * theta1dot_raw
     theta2dot = 2 * math.pi * gear_ratio * theta2dot_raw
     theta3dot = 2 * math.pi * gear_ratio * theta3dot_raw
@@ -100,15 +107,21 @@ while True:
     theta2 = 2 * math.pi * gear_ratio * (hip_2_zero_offset - theta2_raw)
     theta3 = 2 * math.pi * gear_ratio * (hip_1_zero_offset - theta3_raw)
     theta4 = 2 * math.pi * gear_ratio * (knee_zero_offset - theta4_raw)
-
-    sock.sendto("{theta1}|{theta2}|{theta3}|{theta4}|0|{theta1dot}|{theta2dot}|{theta3dot}|{theta4dot}|0".format(theta1=theta1, theta2=theta2, theta3=theta3, theta4=theta4, theta1dot=theta1dot, theta2dot=theta2dot, theta3dot=theta3dot, theta4dot=theta4dot).encode(), (UDP_IP, UDP_PORT))
-    print("after send")
-    # data, addr = sock.recvfrom(4096) # buffer size is 1024 bytes
+    
+    message = "{theta1}|{theta2}|{theta3}|{theta4}|0|{theta1dot}|{theta2dot}|{theta3dot}|{theta4dot}|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0".format(theta1=theta1, theta2=theta2, theta3=theta3, theta4=theta4, theta1dot=theta1dot, theta2dot=theta2dot, theta3dot=theta3dot, theta4dot=theta4dot)
+    sock.sendto(message.encode(), (UDP_IP, UDP_PORT))
+    
+    # data, addr = sock.recvfrom(4096) # buffer size is 4096 bytes
     # print("received message: %s" % data)
 
+    # tau_raw = [float(x) for x in data.decode().split("|")[:-1]]
+
+    # tau_1 = tau_raw[0] * gear_ratio
+    # tau_2 = tau_raw[1] * gear_ratio
+    # tau_3 = tau_raw[2] * gear_ratio
+    # tau_4 = tau_raw[3] * gear_ratio
 
     if out_of_bounds(hip_3_lower_limit, hip_3_upper_limit, theta1) or out_of_bounds(hip_2_lower_limit, hip_2_upper_limit, theta2) or out_of_bounds(hip_1_lower_limit, hip_1_upper_limit, theta3) or out_of_bounds(knee_lower_limit, knee_upper_limit, theta4):
-        # print("hip3 before axis idle:", hip_3_servo.axis0.pos_vel_mapper.pos_rel)
         if hip_3_servo.axis0.current_state != AXIS_STATE_IDLE:
             hip_3_servo.axis0.requested_state = AXIS_STATE_IDLE
 
@@ -121,12 +134,9 @@ while True:
         if knee_servo.axis0.current_state != AXIS_STATE_IDLE:
             knee_servo.axis0.requested_state = AXIS_STATE_IDLE
 
-        # hip_3_servo.axis0.requested_state = hip_2_servo.axis0.requested_state = hip_1_servo.axis0.requested_state = knee_servo.axis0.requested_state = AXIS_STATE_IDLE
-        # print("hip3 after axis idle:", hip_3_servo.axis0.pos_vel_mapper.pos_rel)
-
         print("Angles out of safe bounds, deactivating all servos!")
 
-    # print("Hip3:", theta1, ", Hip2:", theta2, ", Hip1:", theta3, ", Knee:", theta4)
+    print("Hip3:", theta1, ", Hip2:", theta2, ", Hip1:", theta3, ", Knee:", theta4)
     # print("Hip3_raw:", theta1_raw, ", Hip2_raw:", theta2_raw, ", Hip1_raw:", theta3_raw, ", Knee_raw:", theta4_raw)
     # print("Hip3:", math.degrees(theta1), ", Hip2:", math.degrees(theta2), ", Hip1:", math.degrees(theta3), ", Knee:", math.degrees(theta4))
 
